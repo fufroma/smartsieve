561a88466830e45cf0311cd7a8baa472e0072dc1
stephengrier <stephengrier@1a25c93d-b13c-0410-b832-9fcae3185438>
2003-01-28 23:31:39 +0000

    smartsieve.css stylesheet replaced by css.php and style.php.

---
+++ conf/style.php
@@ -0,0 +1,159 @@
+<?php
+/*
+ * This file holds the SmartSieve stylesheet configuration.
+ *
+ * $Id$
+ */
+
+
+$css['body']['background-color'] = '#e5e5e5';
+$css['body']['font-family'] = 'helvetica,sans-serif';
+$css['body']['font-size'] = '10pt';
+$css['body']['color'] = '#000000';
+
+$css['input']['font-family'] = 'helvetica,sans-serif';
+$css['input']['font-size'] = '10pt';
+$css['input']['color'] = '#000000';
+
+$css['a:link']['color'] = '#0000FF';
+$css['a:link']['font-family'] = 'helvetica,sans-serif';
+$css['a:link']['font-size'] = '10pt';
+$css['a:link']['text-decoration'] = 'none';
+
+$css['a:visited']['color'] = '#0000FF';
+$css['a:visited']['font-family'] = 'helvetica,sans-serif';
+$css['a:visited']['font-size'] = '10pt';
+$css['a:visited']['text-decoration'] = 'none';
+
+$css['a:active']['color'] = '#0000FF';
+$css['a:active']['font-family'] = 'helvetica,sans-serif';
+$css['a:active']['font-size'] = '10pt';
+$css['a:active']['text-decoration'] = 'none';
+
+$css['a:hover']['color'] = '#FFFFFF';
+$css['a:hover']['font-family'] = 'helvetica,sans-serif';
+$css['a:hover']['font-size'] = '10pt';
+$css['a:hover']['text-decoration'] = 'none';
+
+$css['a.rule:link']['color'] = '#0000FF';
+$css['a.rule:link']['font-family'] = 'sans-serif';
+$css['a.rule:link']['font-size'] = '8pt';
+$css['a.rule:link']['text-decoration'] = 'none';
+
+$css['a.rule:visited']['color'] = '#0000FF';
+$css['a.rule:visited']['font-family'] = 'sans-serif';
+$css['a.rule:visited']['font-size'] = '8pt';
+$css['a.rule:visited']['text-decoration'] = 'none';
+
+$css['a.rule:active']['color'] = '#000000';
+$css['a.rule:active']['font-family'] = 'sans-serif';
+$css['a.rule:active']['font-size'] = '8pt';
+$css['a.rule:active']['text-decoration'] = 'none';
+
+$css['a.rule:hover']['color'] = '#0000FF';
+$css['a.rule:hover']['font-family'] = 'sans-serif';
+$css['a.rule:hover']['font-size'] = '8pt';
+$css['a.rule:hover']['text-decoration'] = 'none';
+
+$css['.active']['background-color'] = '#b5b5b5';
+$css['.active']['color'] = '#008000';
+$css['.active']['font-family'] = 'helvetica,sans-serif';
+$css['.active']['font-size'] = '10pt';
+$css['.active']['font-weight'] = 'bold';
+
+$css['.inactive']['background-color'] = '#b5b5b5';
+$css['.inactive']['color'] = '#FF0000';
+$css['.inactive']['font-family'] = 'helvetica,sans-serif';
+$css['.inactive']['font-size'] = '10pt';
+$css['.inactive']['font-weight'] = 'bold';
+
+$css['.enabled']['color'] = '#008000';
+$css['.enabled']['font-family'] = 'helvetica,sans-serif';
+$css['.enabled']['font-size'] = '9pt';
+$css['.enabled']['background-color'] = '#FFFFFF';
+
+$css['.disabled']['color'] = '#FF0000';
+$css['.disabled']['font-family'] = 'helvetica,sans-serif';
+$css['.disabled']['font-size'] = '9pt';
+$css['.disabled']['background-color'] = '#FFFFFF';
+
+$css['.ruleenabled']['color'] = '#008000';
+$css['.ruleenabled']['font-family'] = 'helvetica,sans-serif';
+$css['.ruleenabled']['font-size'] = '9pt';
+$css['.ruleenabled']['background-color'] = '#e5e5e5';
+
+$css['.ruledisabled']['color'] = '#FF0000';
+$css['.ruledisabled']['font-family'] = 'helvetica,sans-serif';
+$css['.ruledisabled']['font-size'] = '9pt';
+$css['.ruledisabled']['background-color'] = '#e5e5e5';
+
+$css['.menu']['background-color'] = '#b5b5b5';
+
+$css['.menuouter']['background-color'] = '#000000';
+
+$css['.main']['background-color'] = '#b5b5b5';
+
+$css['.status']['background-color'] = '#b5b5b5';
+$css['.status']['color'] = '#000000';
+$css['.status']['font-family'] = 'helvetica,sans-serif';
+$css['.status']['font-size'] = '10pt';
+$css['.status']['font-weight'] = 'bold';
+
+$css['.statusouter']['background-color'] = '#000000';
+
+$css['.heading']['background-color'] = '#e5e5e5';
+$css['.heading']['color'] = '#000000';
+$css['.heading']['font-family'] = 'helvetica,sans-serif';
+$css['.heading']['font-size'] = '9pt';
+$css['.heading']['font-weight'] = 'bold';
+
+$css['.ruleinfo']['background-color'] = '#e5e5e5';
+$css['.ruleinfo']['color'] = '#000000';
+$css['.ruleinfo']['font-family'] = 'helvetica,sans-serif';
+$css['.ruleinfo']['font-size'] = '10pt';
+$css['.ruleinfo']['font-weight'] = 'bold';
+
+$css['.rules']['background-color'] = '#FFFFFF';
+
+$css['.options']['background-color'] = '#b5b5b5';
+$css['.options']['color'] = '#0000FF';
+$css['.options']['font-family'] = 'helvetica,sans-serif';
+$css['.options']['font-size'] = '9pt';
+
+$css['.errors']['background-color'] = '#FFFFFF';
+$css['.errors']['color'] = '#FF0000';
+$css['.errors']['font-family'] = 'helvetica,sans-serif';
+$css['.errors']['font-size'] = '9pt';
+
+$css['.messages']['background-color'] = '#FFFFFF';
+$css['.messages']['color'] = '#000000';
+$css['.messages']['font-family'] = 'helvetica,sans-serif';
+$css['.messages']['font-size'] = '9pt';
+
+$css['.welcome']['background-color'] = '#b5b5b5';
+$css['.welcome']['color'] = '#000000';
+$css['.welcome']['font-family'] = 'helvetica,sans-serif';
+$css['.welcome']['font-size'] = '9pt';
+$css['.welcome']['font-weight'] = 'bold';
+
+$css['a.option:link']['color'] = '#0000FF';
+$css['a.option:link']['font-family'] = 'helvetica,sans-serif';
+$css['a.option:link']['font-size'] = '9pt';
+$css['a.option:link']['text-decoration'] = 'none';
+ 
+$css['a.option:visited']['color'] = '#0000FF';
+$css['a.option:visited']['font-family'] = 'helvetica,sans-serif';
+$css['a.option:visited']['font-size'] = '9pt';
+$css['a.option:visited']['text-decoration'] = 'none';
+ 
+$css['a.option:active']['color'] = '#000000';
+$css['a.option:active']['font-family'] = 'helvetica,sans-serif';
+$css['a.option:active']['font-size'] = '9pt';
+$css['a.option:active']['text-decoration'] = 'none';
+ 
+$css['a.option:hover']['color'] = '#0000FF';
+$css['a.option:hover']['font-family'] = 'helvetica,sans-serif';
+$css['a.option:hover']['font-size'] = '9pt';
+$css['a.option:hover']['text-decoration'] = 'none';
+
+?>
---
+++ css.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * $Id$
+ *
+ * Copyright 2002 Stephen Grier <stephengrier@users.sourceforge.net>
+ *
+ * See the inclosed NOTICE file for conditions of use and distribution.
+ */
+
+
+require './conf/config.php';
+include "$default->config_dir/style.php";
+
+
+header('Content-type: text/css');
+header('Cache-Control: must-revalidate');
+
+if (is_array($css)){
+    foreach ($css as $class => $attributes){
+        echo $class . "{\n";
+        if (is_array($attributes)){
+            foreach ($attributes as $name => $val){
+                echo "\t$name: $val;\n";
+            }
+        }
+        echo "}\n\n";
+    }
+}
+
+
