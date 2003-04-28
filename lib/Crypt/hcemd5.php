dee17cd1701c4c8038715560a54723a423e109ff
stephengrier <stephengrier@1a25c93d-b13c-0410-b832-9fcae3185438>
2003-04-28 16:32:12 +0000

    * Re-worked SmartSieve's cryptographic handling:  - abstracted the code which interfaces with external cryptographic libraries into    a new SmartSieveCrypt class. SmartSieveCrypt::factory() instantiates a sub-class    which interfaces with the chosen cryptographic library. Sub-classes can be found    in lib/Crypt/. Encrypt::getCryptLib() decides which library SmartSieve should use.  - The Encrypt class now instantiates a SmartSieveCrypt object to do the encryption    decryption work.  - Added support for PEAR's Crypt_Rc4 library.

---
+++ lib/Crypt/hcemd5.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * $Id$
+ *
+ * Copyright 2002 Stephen Grier <stephengrier@users.sourceforge.net>
+ *
+ * See the inclosed NOTICE file for conditions of use and distribution.
+ */
+
+
+/*
+ * class SmartSieveCryptHCEMD5 extends the SmartSieveCrypt class.
+ * It allows SmartSieve to use the PEAR HCEMD5 library for encryption.
+ */
+
+class SmartSieveCryptHCEMD5 extends SmartSieveCrypt {
+
+
+    /* The hcemd5 object reference. */
+    var $hcemd5;
+
+
+    /* constructor. */
+    function SmartSieveCryptHCEMD5 ($args = array()) 
+    {
+        require_once 'Crypt/HCEMD5.php';
+        $this->hcemd5 = new Crypt_HCEMD5($args['key']);
+    }
+
+
+    function encrypt ($string) 
+    {
+	$encrypted = $this->hcemd5->encodeMimeSelfRand($string);
+	return $encrypted;
+    }
+
+
+    function decrypt ($string) 
+    {
+	$decrypted = $this->hcemd5->decodeMimeSelfRand($string);
+	return $decrypted;
+    }
+
+
+}
+
+
+?>
