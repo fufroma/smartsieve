5ba7c67cb0806601355836342c3e8babea19cd2f
stephengrier <stephengrier@1a25c93d-b13c-0410-b832-9fcae3185438>
2003-08-18 16:35:21 +0000

    status and messages rows on all pages moved to new common_status.inc include file.

--- smartsieve/trunk/rule.php
+++ smartsieve/trunk/rule.php
@@ -172,62 +172,9 @@ if (!empty($default->rule_help_url)){
 
 include $default->include_dir . '/common-head.inc';
 include $default->include_dir . '/menu.inc';
+include $default->include_dir . '/common_status.inc';
 
 ?>
-
-<BR>
-<?php if ($errors || $msgs) {  ?>
-
-<TABLE WIDTH="100%" CELLPADDING="5" BORDER="0" CELLSPACING="0">
-<?php foreach ($errors as $err){ ?>
-  <TR>
-    <TD CLASS="errors">
-      <?php print "$err\n"; ?>
-    </TD>
-  </TR>
-<?php } ?>
-<?php foreach ($msgs as $msg){ ?>
-  <TR>
-    <TD CLASS="messages">
-      <?php echo "$msg\n"; ?>
-    </TD>
-  </TR>
-<?php } ?>
-</TABLE>
-
-<BR>
-<?php } //end if $errors ?>
-
-<TABLE WIDTH="100%" CELLPADDING="1" BORDER="0" CELLSPACING="0">
-<TR>
-  <TD CLASS="statusouter">
-    <TABLE WIDTH="100%" CELLPADDING="2" BORDER="0" CELLSPACING="0">
-      <TR CLASS="status">
-        <TD>
-          &nbsp;User: <?php print $sieve->user; ?>
-        </TD>
-        <TD>
-          &nbsp;Server: <?php print $sieve->server; ?>
-        </TD>
-        <TD>
-          &nbsp;Script: <?php print $sieve->workingscript; ?>
-        </TD>
-<?php if (AppSession::isActiveScript($sieve->workingscript)) { ?>
-        <TD CLASS="active">
-          ACTIVE
-        </TD>
-<?php } else { ?>
-        <TD CLASS="inactive">
-          NOT ACTIVE
-        </TD>
-<?php } ?>
-      </TR>
-    </TABLE>
-  </TD>
-</TR>
-</TABLE>
-
-<BR>
 
 <FORM ACTION="<?php print AppSession::setUrl('rule.php');?>" METHOD="post" NAME="thisRule">
 
