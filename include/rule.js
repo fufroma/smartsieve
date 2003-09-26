ccbe20dc471175fe4e3db09296196b15aca03dfd
stephengrier <stephengrier@1a25c93d-b13c-0410-b832-9fcae3185438>
2003-09-26 16:45:42 +0000

    added a javascript confirmation popup when a match string contains wildcard chars with $default->websieve_auto_matches = true to warn them that :matches will be used.

--- include/rule.js
+++ include/rule.js
@@ -17,6 +17,15 @@ function Submit(a)
                 return true;
         }
     }
+<?php if ($default->websieve_auto_matches){ ?>
+    for (i = 0; i < document.thisRule.elements.length; i++) {
+        if (window.document.thisRule.elements[i].value.indexOf('*') != -1 ||
+            window.document.thisRule.elements[i].value.indexOf('?') != -1){
+            if (!confirm("Your match string contains a '*' or a '?' character.\nThese will be interpreted as wildcard characters rather than literals.\nIs this OK? If not, click Cancel and use the regex option."))
+                return false;
+        }
+    }
+<?php } ?>
     document.thisRule.thisAction.value = a;
     document.thisRule.submit();
 }
