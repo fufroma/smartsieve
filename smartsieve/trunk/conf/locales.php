72d534d4071d839f2e52d1de853882f906497bbe
stephengrier <stephengrier@1a25c93d-b13c-0410-b832-9fcae3185438>
2004-03-26 12:02:24 +0000

    - Add conf/locales.php. The language select widget on the login page is now   generated from entries in this file. - Some cleanups to login page template.

---
+++ smartsieve/trunk/conf/locales.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This is the SmartSieve i18n configuration file.
+ * You can enable/disable languages by commenting out the appropriate 
+ * entries below. New languages will need at least a display value.
+ *
+ * $Id$
+ */
+
+$locales['ca_ES']['display'] = 'Catal&agrave;';
+$locales['ca_ES']['charset'] = 'ISO-8859-15';
+
+$locales['de_DE']['display'] = 'Deutsch';
+$locales['de_DE']['charset'] = 'ISO-8859-1';
+
+$locales['en_GB']['display'] = 'English (GB)';
+$locales['en_GB']['charset'] = 'ISO-8859-1';
+
+$locales['es_ES']['display'] = 'Espa&ntilde;ol';
+$locales['es_ES']['charset'] = 'ISO-8859-15';
+
+$locales['it_IT']['display'] = 'Italiano';
+$locales['it_IT']['charset'] = 'ISO-8859-15';
+
+?>
