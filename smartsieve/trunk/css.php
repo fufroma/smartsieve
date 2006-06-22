5e6b958dc083e53105f0b837a713eadbfac21ed5
stephengrier <stephengrier@1a25c93d-b13c-0410-b832-9fcae3185438>
2006-06-22 15:39:36 +0000

    Add cache control headers to style sheet, and cache by default.

--- smartsieve/trunk/css.php
+++ smartsieve/trunk/css.php
@@ -9,11 +9,20 @@
 
 
 require './conf/config.php';
-include "$default->config_dir/style.php";
+include "$default->lib_dir/SmartSieve.lib";
+require SmartSieve::getConf('config_dir', 'conf') . '/style.php';
 
 
 header('Content-type: text/css');
-header('Cache-Control: must-revalidate');
+if (SmartSieve::getConf('cache_css') !== false) {
+    $mtime = filemtime(SmartSieve::getConf('config_dir', 'conf') . '/style.php');
+    $last_mod = sprintf('%s GMT', gmdate('D, d M Y H:i:s', $mtime));
+    header('Last-Modified: ' . $last_mod);
+    header('Cache-Control: max-age=86400, public, must-revalidate');
+} else {
+    header('Cache-Control: no-cache, no-store, must-revalidate');
+    header('Expires: -1');
+}
 
 if (is_array($css)){
     foreach ($css as $class => $attributes){
